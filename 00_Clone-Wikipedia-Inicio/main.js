d4b8197b25c78f9f62c08ff6b987d350524a97d6
Daniel Montiel <danielponce1397@gmail.com>
2024-02-19 17:42:33 -0600

    Se detallaron de pagina wikipedia

--- 00_Clone-Wikipedia-Inicio/main.js
+++
@@ -1,6 +0,0 @@
-
-function mostrarAbreviacion() {
-    const select = document.getElementById("idioma");
-    let opcionSeleccionada = select.options[select.selectedIndex];
-    opcionSeleccionada.text = opcionSeleccionada.value;
-}
